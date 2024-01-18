3950072c891404f8a3bd7726cc337533e3d58db8
Syed Rizwan Sadiq K <145868568+userisRizz@users.noreply.github.com>
2024-01-19 02:30:56 +0530

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+      <span>© Syed Rizwan sk</span>
     </div>
   )
 }
